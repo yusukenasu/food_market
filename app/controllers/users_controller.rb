8adc7b5da6e55d17091c5af4e51efc2ac1e734e4
那須裕介 <nasuyusuke@MacBook-Pro.local>
2023-06-24 20:12:06 +0900

    [add]プロフィール編集機能を追加
    [add]ActiveStorageを導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
-  def index
+  def home
   end
+
+  def edit_account
+    @user = current_user
+  end
+
+  def account_update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to users_home_path
+    else
+      render "users/edit_account"
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:profile, :image, :name)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
-  root 'users#index'
-  get 'users/index' ,to: 'users#index'
-  
+
+  root 'users#home'
+  get 'users/home' ,to: 'users#home'
+  get 'users/edit_account' ,to: 'users#edit_account'
+  patch 'users/account_update' ,to: 'users#account_update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
