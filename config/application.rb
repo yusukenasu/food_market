02a4c24dd4b631cbf98fd82c8347bc7c23d78a81
那須裕介 <nasuyusuke@MacBook-Pro.local>
2023-07-03 21:18:51 +0900

    [update]deviseの日本語表示をi18nを使用した形式に修正

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module FoodMarket
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_storage.variant_processor = :mini_magick
+    config.i18n.default_locale = :ja
   end
 end
