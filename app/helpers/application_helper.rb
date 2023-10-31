f15a95b41ac044a176c8023e30179afb90f3445c
那須裕介 <nasuyusuke@MacBook-Pro.local>
2023-10-31 22:26:59 +0900

    [update]商品の評価ポイントを星マークでの表示に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def item_score(score)
+    star = ""
+    score.times do 
+      star += "★"
+    end
+    (5-score).times do
+      star += "☆"
+    end
+    return star
+  end
 end
