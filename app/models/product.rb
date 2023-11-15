67bae05bacf0c6aad1d3b76b661b8dc53159794b
那須裕介 <nasuyusuke@MacBook-Pro.local>
2023-11-15 22:13:17 +0900

    [add]User, Product, Favoriteモデルにアソシエーションを記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
   has_one_attached :image
   validates :name, presence: true
   validates :manufacturer, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :products, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
