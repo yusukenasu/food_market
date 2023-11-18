274c73d285ed50f886177b2ceb2c76eda21f11a9
那須裕介 <nasuyusuke@MacBook-Pro.local>
2023-11-18 17:16:49 +0900

    [add]ユーザーランクの表示機能を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,17 @@ module ApplicationHelper
     number = Favorite.where(product_id: product_id)
     return number.count
   end
+
+  def user_rank(user_id)
+    product_id = Product.where(user_id: user_id)
+    number = Favorite.where(product_id: product_id)
+    case number.count
+    when 0..3
+      return "駆け出しユーザー"
+    when 4..5 
+      return "目利きユーザー"
+    else
+      return "プロバイヤー並みユーザー"
+    end
+  end
 end
