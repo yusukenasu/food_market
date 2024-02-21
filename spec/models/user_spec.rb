112074c056710ed9eaa2252caabf242ab16d5d55
那須裕介 <nasuyusuke@MacBook-Pro.local>
2024-02-21 09:16:06 +0900

    [update]Userモデルのモデルスペックを実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,43 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "ユーザー新規登録" do
-    let(:user) { create(:user) }
-      
-      it "ファクトリが有効であること" do
-        expect(user).to be_valid
-      end
+  describe "validations" do
+    # ファクトリが有効であること
+    it "is valid with a factory" do
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
+    end
 
-      it "名前がない場合、無効であること" do
-        user = FactoryBot.build(:user, name: nil)
-        user.valid?
-        expect(user.errors[:name]).to include("を入力してください")
-      end
+    # 名前がない場合、無効であること
+    it "is invalid without a name" do
+      user = FactoryBot.build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
 
-      it "パスワードがない場合、無効であること" do
-        user = FactoryBot.build(:user, password: nil)
-        user.valid?
-        expect(user.errors[:password]).to include("を入力してください")
-      end
+    # パスワードがない場合、無効であること
+    it "is invalid without a password" do
+      user = FactoryBot.build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
 
-      it "メールアドレスがない場合、無効であること" do
-        user = FactoryBot.build(:user, email: nil)
-        user.valid?
-        expect(user.errors[:email]).to include("を入力してください")
-      end
+    # パスワードが５文字以下の場合、無効であること
+    it "is invalid without a password less than 5 charactors " do
+      user = FactoryBot.build(:user, password: "12345")
+      user.valid?
+      expect(user.errors[:password]).to include("は6文字以上で入力してください")
+    end
+    
+    # メールアドレスがない場合、無効であること
+    it "is invalid without a email" do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
 
-      it "重複したメールアドレスの場合無効であること" do
-        user.save
-        another_user = FactoryBot.build(:user, email: user.email)
-        another_user.valid?
-        expect(another_user.errors[:email]).to include("はすでに存在します")
-      end
+    # 重複したメールアドレスの場合無効であること
+    it "is invalid with a duplicate email" do
+      user = FactoryBot.create(:user)
+      another_user = FactoryBot.build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
 
-      it "プロフィールが41文字以上で無効であること" do
-        profile_max_letters = "a" * 41
-        user = FactoryBot.build(:user, profile: profile_max_letters )
-        user.valid?
-        expect(user.errors[:profile]).to include("は40文字以内で入力してください")
-      end
+    # プロフィールが41文字以上で無効であること
+    it "is invalid with over 41 characters in the profile" do
+      profile_max_letters = "a" * 41
+      user = FactoryBot.build(:user, profile: profile_max_letters )
+      user.valid?
+      expect(user.errors[:profile]).to include("は40文字以内で入力してください")
+    end
   end
 end
