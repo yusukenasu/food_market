75c0ec06c21c882bf498323f92a4077c50281290
那須裕介 <nasuyusuke@MacBook-Pro.local>
2024-02-24 21:44:10 +0900

    [update]ユーザー新規登録、ログイン、ログアウトのシステムスペックを実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name {"test"}
-    sequence (:email) { |n| "test#{n}@gmail.com" }
+    sequence (:email) { |n| "test#{n}@example.com" }
     password {"123456"}
     profile {"test"}
   end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,8 +1,89 @@
 RSpec.describe "ユーザーページ", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
   describe "ユーザー新規登録" do
+    context "リンクから新規登録する場合" do
+      before do
+        visit products_path
+        within ".firstview_registration_message" do
+          click_link "新規登録"
+        end
+        fill_in "メールアドレス", with: "test@example.com"
+        fill_in "名前", with: "test"
+        fill_in "パスワード", with: "password"
+        fill_in "パスワード（確認）", with: "password"
+        click_button "登録"
+      end
+      it "ユーザー登録が完了すること" do
+        expect(page).to have_content "アカウント登録が完了しました。"
+      end
+    end
+
+    context "ボタンから新規登録する場合" do
+      before do
+        visit products_path
+        within ".firstview_loggedout_buttons_container" do
+          click_link "新規登録"
+        end
+        fill_in "メールアドレス", with: "test@example.com"
+        fill_in "名前", with: "test"
+        fill_in "パスワード", with: "password"
+        fill_in "パスワード（確認）", with: "password"
+        click_button "登録"
+      end
+      it "ユーザー登録が完了すること" do
+        expect(page).to have_content "アカウント登録が完了しました。"
+      end
+    end
   end 
 
   describe "ユーザーログイン・ログアウト" do
+    let!(:user) { create(:user, email: "test@example.com", password: "password") }
+    
+    context "リンクからログインする場合" do
+      before do
+        visit products_path
+        within ".firstview_registration_message" do
+          click_link "ログイン"
+        end
+        fill_in "メールアドレス", with: "test@example.com"
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+      end
+      it "ユーザーログインが完了すること" do
+        expect(page).to have_content "ログインしました。"
+      end
+    end
+
+    context "ボタンからログインする場合" do
+      before do
+        visit products_path
+        within ".firstview_upper_container" do
+          click_link "ログイン"
+        end
+        fill_in "メールアドレス", with: "test@example.com"
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+      end
+      it "ユーザーログインが完了すること" do
+        expect(page).to have_content "ログインしました。"
+      end
+    end
+
+    context "ログアウトする場合" do
+      before do
+        login_as(user, scope: :user)
+        visit products_path
+        within ".dropdown-menu" do
+          click_link "ログアウト" 
+        end
+      end
+      it "ユーザーがログアウトすること" do
+        expect(page).to have_content "ログアウトしました。"
+      end
+    end
   end
 
   describe "ユーザープロフィール編集" do
