dbc5a12e0585aa019d67b0c863e0eb1ba459f453
那須裕介 <nasuyusuke@MacBook-Pro.local>
2024-03-03 18:35:51 +0900

    [update]商品詳細画面の表示についてシステムスペックを実装

--- spec/system/product_spec.rb
+++ spec/system/product_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe "Products", type: :system do
   end
   
   describe "商品情報の登録" do
-    let(:user) { create(:user) }
-    
+    let!(:user) { create(:user) }
+   
     before do
       login_as(user, scope: :user)
       visit products_path
@@ -25,7 +25,13 @@ RSpec.describe "Products", type: :system do
       select "5", from: "デザイン性 ※"
       click_button "登録する"
     end
-    it "商品登録が完了すること" do
+
+    it "登録完了後、商品詳細ページに遷移すること" do
+      new_product = Product.last
+      expect(current_path).to eq(product_path(new_product))
+    end
+
+    it "登録された商品情報が表示されること" do
       expect(page).to have_content "new_product_name"
       expect(page).to have_content "new_product_manufacturer"
       expect(page).to have_content "200"
@@ -40,8 +46,86 @@ RSpec.describe "Products", type: :system do
     end
   end
 
-  describe "商品詳細の表示" do
+  describe "商品詳細画面の表示" do
+    let!(:user) { create(:user) }
+    let(:product) { create(:product, :product_with_image) }
+
+    before do
+      login_as(user, scope: :user)
+      visit product_path(product)
+    end
+
+    it "商品情報が表示されること" do
+      expect(page).to have_content "product_name"
+      expect(page).to have_content "product_manufacturer"
+      expect(page).to have_content "100"
+      expect(page).to have_content "product_category"
+      expect(page).to have_selector("img[src*='product_image_1.jpeg']")
+    end
+
+    it "商品評価、レーダーチャートが表示されること" do
+      expect(page).to have_css("canvas#myChart")
+      expect(page).to have_content "コスパ★★★★★"
+      expect(page).to have_content "特徴的★★★★☆"
+      expect(page).to have_content "味★★★☆☆"
+      expect(page).to have_content "リピート性★★☆☆☆"
+      expect(page).to have_content "デザイン性★☆☆☆☆"
+    end
+    
+    it "商品投稿者情報、商品説明が表示されること" do
+      save_and_open_page
+      expect(page).to have_css(".contributor_user_rank")
+      expect(page).to have_content "test_name"
+      expect(page).to have_selector("img[src*='user_image_before.png']")
+      expect(page).to have_content "product_description"
+    end
   end
+
+  describe "商品詳細ページの関連商品" do
+    let!(:user) { create(:user) }
+   
+    before do
+      login_as(user, scope: :user)
+    end
+
+    context "関連商品が存在する場合" do
+      before do
+        visit new_product_path
+        fill_in "商品名 ※", with: "マヨネーズ"
+        fill_in "製造メーカー ※", with: "キューピー"
+        fill_in "円・価格（税込） ※", with: "200"
+        attach_file "商品画像 ※", 'spec/fixtures/product_image_1.jpeg'
+        select "1", from: "コスパ ※"
+        select "2", from: "特徴的 ※"
+        select "3", from: "味 ※"
+        select "4", from: "リピート性 ※"
+        select "5", from: "デザイン性 ※"
+        click_button "登録する"
+      end
+
+      it "関連商品の名前、画像、価格が表示されること" do
+        expect(page).to have_css(".item_name")
+        expect(page).to have_css(".item_image")
+        expect(page).to have_css(".item_price")
+      end
+
+      it "関連商品が4つ表示されること" do
+        all_related_items = page.all(".item_container")
+
+        expect(all_related_items.count).to eq 4
+      end
+    end
+
+    context "関連商品が存在しない場合" do      
+      let!(:product) { create(:product, :product_with_image) }
+
+      it "関連商品が表示されないこと" do
+        visit product_path(product)
+        expect(page).to have_content("※関連商品は見つかりませんでした。")
+      end
+    end
+  end
+
 
   describe "登録商品の編集" do
   end
